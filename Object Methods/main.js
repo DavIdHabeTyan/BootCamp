af3cc7e2ab2c878ba22efde8849ad0fda4d3ba4b
DavIdHabeTyan <davidhabetyanm@gmail.com>
2022-07-31 12:04:44 +0400

    added Object.methods-Objcet.create()

--- Object Methods/main.js
+++ Object Methods/main.js
@@ -1,5 +1,8 @@
 
 
+
+// Object.assign()
+
 let user = {
    name: "Dav",
 }
@@ -10,3 +13,24 @@ let result = Object.defineProperty(user, "lastName", {
 })
 
 console.log(result)
+
+
+// ------- Object.create() ---
+
+const human = {
+   name: "John",
+
+   sayName: function () {
+      console.log(this.name);
+   }
+};
+
+const developer = Object.create(human);
+developer.name = "Developer";
+
+developer.writeCode = () => {
+   console.log("writing code");
+}
+
+developer.writeCode();
+developer.sayName();
