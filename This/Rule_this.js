07ed7a78d826173ef6379c3d327fa68d95e52ed3
David Habetyan <davidhabetyanm@gmail.com>
2022-08-01 23:26:29 +0400

    this experiment testing

--- This/Rule_this.js
+++ This/Rule_this.js
@@ -28,3 +28,5 @@ class Foo{
 
 const foo = new Foo();
 console.log(foo)
+
+
--- This/this.js
+++ This/this.js
@@ -2,17 +2,48 @@
 // inq@ paymanavorvaca vochte ira stexcman texov, ayl kanchi texov,
 
 
-function foo() {
-   console.log(this)
-}
-
-let that = {
-   name: "Dav",
-   boo: foo,
-   goo: function(){
-      foo();
-   }
-}
+// function foo() {
+//    console.log(this)
+// }
+//
+// let that = {
+//    name: "Dav", boo: foo, goo: function () {
+//       foo();
+//    }
+// }
 
 // console.log(that.boo());
-that.goo()
+// that.goo()
+
+
+// const calc = {
+//    num: 0,
+//    increment() {
+//       console.log(this === calc); // => true
+//       this.num += 1;
+//       return this.num;
+//    }
+// };
+// // method invocation. this is calc
+// calc.increment(); // => 1
+// calc.increment();
+
+class Foo {
+   bar = () => {
+      console.log(this);
+   };
+}
+const foo = new Foo()
+console.log(foo)
+
+const testArrow = () => {
+   console.log(this);
+
+};
+
+console.log(testArrow)
+
+
+
+
+
