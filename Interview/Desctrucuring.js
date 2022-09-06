c723872c0354ee0fd6ebef4173228d25da4b1c93
David Habetyan <davidhabetyanm@gmail.com>
2022-09-06 23:54:36 +0400

    added Interview question destructoring

---
+++ Interview/Desctrucuring.js
@@ -0,0 +1,19 @@
+// --- Objecti meji key@ vekalenq bayc urish popoxakan veragrenq
+
+
+const people = {
+   name: "David",
+   age : 35,
+   phone: 45715454,
+   info: {
+      address: "Yerevan"
+   }
+}
+
+const {name: otherName} = people;
+
+console.log(otherName)  // =>  "David";
+
+
+
+
